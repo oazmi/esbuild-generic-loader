07aec00401c7fff6f9a8f07572490a5835dcec5c
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-11-29 23:17:41 -0500

    - add a few missing doc comments to boost jsr score. - in `/examples/1_html/build.ts`:   - add option for dry-running the build script (no writes done to filesystem).   - make sure that the output directory is first emptied.

--- examples/1_html/build.ts
+++ examples/1_html/build.ts
@@ -1,4 +1,5 @@
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.11.0"
+import { emptyDir } from "jsr:@std/fs"
 import { fromFileUrl } from "jsr:@std/path"
 import esbuild from "npm:esbuild"
 import { resolveAsUrl } from "../../src/deps.ts"
@@ -7,6 +8,7 @@ import { HtmlLoader } from "./loader.ts"
 
 
 const
+	dryrun = false,
 	this_dir_path = resolveAsUrl("./", import.meta.url),
 	html_file_path = resolveAsUrl("./input/index.html", this_dir_path),
 	html_file_content = await (await fetch(html_file_path)).text()
@@ -46,13 +48,16 @@ other_output_files.forEach((js_file, index) => {
 	console.log(js_file.text)
 })
 
+const abs_output_dir = fromFileUrl(resolveAsUrl("./output/", this_dir_path))
+console.log(`clearing out the output directory: "${abs_output_dir}"`)
+if (!dryrun) { await emptyDir(abs_output_dir) }
 await writeOutputFiles([
 	{
 		path: html_in_js_compiled.path.replace(/\.js$/, ".html"),
 		text: html_compiled_text
 	}, ...other_output_files
 ], {
-	dir: "./output/",
+	dir: abs_output_dir,
 	log: "verbose",
-	dryrun: false,
+	dryrun,
 })
